2ee20c40689eed4459c143cc447860ec542b524d
nanotone <nanotone@gmail.com>
2012-02-26 23:32:57 -0500

    switch to use /info endpoint

--- static/js/spoked.js
+++ static/js/spoked.js
@@ -7,26 +7,17 @@ function processingReady() {
 	instance = Processing.getInstanceById('processing');
 }
 
-function onClickTrack() {
-	var button = $(this);
-	var trackid = button.data('trackid');
-	$.get(SERVER + 'track/' + trackid, function(data) {
-		data = JSON.parse(data);
-		instance.drawRide(data);
-	});
-}
-
 function onClickA(e) {
 	e.preventDefault();
 	var index = $(this).closest('li').index();
-	$.get(SERVER + 'track/' + tracks[index], function(data) {
+	$.get(SERVER + 'track/' + tracks[index].id, function(data) {
 		instance.drawRide(JSON.parse(data), index);
 	});
 }
 
 $(function() {
-	$.get(SERVER + 'tracks', function(data) {
-		tracks = JSON.parse(data);
+	$.get(SERVER + 'info', function(data) {
+		tracks = JSON.parse(data).tracks;
 	});
 	$('#portrait-nav a').click(onClickA);
 });
