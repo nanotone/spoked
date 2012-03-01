55df515242a81eee240dc52e329d34c0236aa22e
nanotone <nanotone@gmail.com>
2012-03-01 13:35:24 -0500

    user avatars are hooked up to the DB

--- static/js/spoked.js
+++ static/js/spoked.js
@@ -7,6 +7,22 @@ function processingReady() {
 	instance = Processing.getInstanceById('processing');
 }
 
+function showPortraits(users) {
+	var template = $('#portrait-template');
+	for (var i = 0; i < users.length; i++) {
+		var user = users[i];
+		var instance = template.clone().removeClass('template').attr('id', null).css({display: ''});
+		var name = user.name;
+		var slug = name.replace(' ', '').toLowerCase();
+		var color = '#' + (user.color || '2dddd2');
+		instance.find('.avatarLink').click(onClickA);
+		instance.find('.avatar').attr('src', 'img/avatar/' + slug + '.jpg').css({borderColor: color});
+		instance.find('.pop').css({backgroundColor: color});
+		instance.find('.name').text(name);
+		template.parent().append(instance);
+	}
+}
+
 function onClickA(e) {
 	e.preventDefault();
 	var index = $(this).closest('li').index();
@@ -16,9 +32,11 @@ function onClickA(e) {
 }
 
 $(function() {
+	$('.template').css({display: 'none'});
 	$.get(SERVER + 'info', function(data) {
-		tracks = JSON.parse(data).tracks;
+		data = JSON.parse(data);
+		tracks = data.tracks;
+		showPortraits(data.users);
 	});
-	$('#portrait-nav a').click(onClickA);
 });
 
