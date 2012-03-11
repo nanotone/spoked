a2c8e50ca043d89b0a274c6bdd17d44e99b530cc
nanotone <nanotone@gmail.com>
2012-03-11 01:49:56 -0500

    fix bug where sunday was considered part of the following week

--- static/js/time.js
+++ static/js/time.js
@@ -5,7 +5,7 @@
 	d.setMinutes(0);
 	d.setSeconds(0);
 	var midnight = Math.floor(d.getTime() / 1000);
-	window.thisWeek = midnight - (d.getDay() - 1) * day;
+	window.thisWeek = midnight - ((d.getDay() + 6) % 7) * day;
 	window.lastWeek = thisWeek - 7 * day;
 })();
 
