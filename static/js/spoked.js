6985fd364764b48906d5f3e64dba91f780f6821f
nanotone <nanotone@gmail.com>
2012-03-10 19:32:14 -0500

    filtering by week, and much improved js separation

--- static/js/spoked.js
+++ static/js/spoked.js
@@ -1,29 +1,9 @@
-var SERVER = 'http://dev.iamspoked.com/';
-
 var instance = null;
-var tracks = null;
-var users = null;
-var usersById = {};
-var tracksById = {};
 var infoPromise = $.Deferred();
 var pdePromise = $.Deferred();
 var initPromise = $.when(infoPromise, pdePromise);
 
 var currentUser = null;
-
-(function() {
-	var day = 24 * 3600;
-	var d = new Date();
-	d.setHours(0);
-	d.setMinutes(0);
-	d.setSeconds(0);
-	var midnight = Math.floor(d.getTime() / 1000);
-	window.thisWeek = midnight - (d.getDay() - 1) * day;
-	window.lastWeek = thisWeek - 7 * day;
-})();
-function getTime() {
-	return (new Date().getTime()) / 1000;
-}
 
 function processingReady() {
 	instance = Processing.getInstanceById('processing');
@@ -89,7 +69,7 @@ function loadState() {
 		$('.friendsLink').closest('li').addClass('selectedLink');
 		instance.abortRideAnimations();
 		instance.background('#ffffff', 0);
-		drawTracks(users);
+		rideCanvas.update({'users': users});
 		$('.compare').hide();
 	}
 	else if (title == 'you') {
@@ -104,7 +84,9 @@ function loadState() {
 	else if (title.length == 49) {
 		instance.abortRideAnimations();
 		instance.background('#ffffff', 0);
-		drawTracks([usersById[title.substr(0, 24)], usersById[title.substr(25)] ]);
+		var user1 = usersById[title.substr(0, 24)];
+		var user2 = usersById[title.substr(25)];
+		rideCanvas.update({'users': [user1, user2]});
 		$('.compare').hide();
 	}
 }
@@ -119,75 +101,9 @@ function showProfile(user) {
 			compareDivs.eq(i).hide();
 		}
 	}
-	drawTracks([user]);
+	rideCanvas.update({'users': [user]});
 }
 
-var activeTrackCollection = {
-	tracks: [],
-	setTracks: function(tracks) {
-		this.tracks = tracks;
-	},
-	drawImmediately: function() {
-		instance.abortRideAnimations();
-		instance.background('#ffffff', 0);
-		var self = this;
-		var i = 0;
-		var drawRideImmediately = function() {
-			var begin = getTime();
-			while (i < self.tracks.length && getTime() - begin < 0.05) {
-				instance.drawRideImmediately(self.tracks[i]);
-				i++;
-			}
-			if (i < self.tracks.length) {
-				setTimeout(drawRideImmediately, 0);
-			}
-		};
-		drawRideImmediately();
-	},
-	drawAnimated: function() {
-		instance.abortRideAnimations();
-		instance.background('#ffffff', 0);
-		instance.animateRides(this.tracks, lastWeek);
-	}
-};
-
-function drawTracks(users, callback) {
-	var tracksToShow = [];
-	for (var i = 0; i < users.length; i++) {
-		var user = users[i];
-		for (var j = 0; j < user.tracks.length; j++) {
-			if (user.tracks[j].time > lastWeek) {
-				tracksToShow.push(user.tracks[j]);
-			}
-		}
-	}
-	fetchTracks(tracksToShow).done(function() {
-		activeTrackCollection.setTracks(tracksToShow);
-		activeTrackCollection.drawImmediately();
-	});
-}
-function fetchTracks(tracks) {
-	var deferred = $.Deferred();
-	var trackIdsToLoad = [];
-	for (var i = 0; i < tracks.length; i++) {
-		if (!tracks[i].points) {
-			trackIdsToLoad.push(tracks[i].id);
-		}
-	}
-	if (trackIdsToLoad.length) {
-		$.get(SERVER + 'tracks?ids=' + trackIdsToLoad.join(','), function(data) {
-			data = JSON.parse(data);
-			for (var trackId in data) {
-				tracksById[trackId].points = data[trackId];
-			}
-			deferred.resolve();
-		});
-	}
-	else {
-		deferred.resolve();
-	}
-	return deferred.promise();
-}
 
 function onClickPortrait(e) {
 	e.preventDefault();
@@ -202,29 +118,11 @@ function onClickCompare(e) {
 }
 
 $(function() {
+	initData();
 	$('.template').css({display: 'none'});
 	infoPromise.done(loadState);
-	$.get(SERVER + 'info', function(data) {
-		data = JSON.parse(data);
-		tracks = data.tracks;
-		users = data.users;
-		for (var i = 0; i < users.length; i++) {
-			users[i].tracks = [];
-			users[i].pjsColor = 0xFF000000 + parseInt(users[i].color, 16);
-			usersById[users[i].id] = users[i];
-		}
-		for (var i = 0; i < tracks.length; i++) {
-			tracksById[tracks[i].id] = tracks[i];
-			var userId = tracks[i].userid;
-			if (userId) {
-				//console.log("userId = " + userId);
-				usersById[userId].tracks.push(tracks[i]);
-				tracks[i].user = usersById[userId];
-			}
-		}
-		showPortraits();
-		infoPromise.resolve();
-	});
+	infoPromise.done(showPortraits);
+
 	$('.friendsLink').click(makeLinkHandler('friends'));
 	$('.youLink').click(makeLinkHandler('you'));
 
@@ -234,14 +132,20 @@ $(function() {
 	$('#animate').click(function(e) {
 		e.preventDefault();
 		var $a = $(this);
-		if ($a.hasClass('selected')) {
-			$a.removeClass('selected');
-			activeTrackCollection.drawImmediately();
-		}
-		else {
-			$a.addClass('selected');
-			activeTrackCollection.drawAnimated();
-		}
+		$a.toggleClass('selected');
+		rideCanvas.redraw();
+	});
+	$('#lastweek').click(function(e) {
+		e.preventDefault();
+		var $a = $(this);
+		$a.toggleClass('selected');
+		rideCanvas.update({minTime: ($a.hasClass('selected') ? lastWeek : thisWeek)});
+	});
+	$('#thisweek').click(function(e) {
+		e.preventDefault();
+		var $a = $(this);
+		$a.toggleClass('selected');
+		rideCanvas.update({maxTime: ($a.hasClass('selected') ? getTime() : thisWeek)});
 	});
 
 	History.Adapter.bind(window, 'statechange', loadState);
---
+++ static/js/time.js
@@ -0,0 +1,14 @@
+(function() {
+	var day = 24 * 3600;
+	var d = new Date();
+	d.setHours(0);
+	d.setMinutes(0);
+	d.setSeconds(0);
+	var midnight = Math.floor(d.getTime() / 1000);
+	window.thisWeek = midnight - (d.getDay() - 1) * day;
+	window.lastWeek = thisWeek - 7 * day;
+})();
+
+function getTime() {
+	return (new Date().getTime()) / 1000;
+}
