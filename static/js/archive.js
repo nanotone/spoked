3508528caaa16e4e9162aa6dae7a217ce8f7e590
nanotone <nanotone@gmail.com>
2012-04-15 18:58:38 -0400

    Team-based games in the archive

--- static/js/archive.js
+++ static/js/archive.js
@@ -31,7 +31,25 @@ function initMain() {
 		$instance.find('.game-smiles').text(humanUnits(Math.round(totalSmiles / M_PER_MI), "smile", true));
 		$instance.find('.game-duration').text(game.humanDuration);
 		if (game.players[0].team) {
-			console.log("teams");
+			var teamsByName = {};
+			var winnerTeam = null;
+			for (var j = 0; j < game.players.length; j++) {
+				var player = game.players[j];
+				var teamName = player.team;
+				if (!teamName) { continue; }
+				var team = teamsByName[teamName];
+				if (!team) {
+					var $node = $('<div class="team"><h4 class="team-name"><mg src="img/icon/' + teamName.toLowerCase() + '.png" />' + teamName + '</h4><h4 class="team-smiles"></h4></div>');
+					team = teamsByName[teamName] = {node: $node, smiles: 0};
+					$instance.find('.game-text').append(team.node);
+				}
+				var user = usersById[player.userid];
+				team.smiles += user.totalSmiles;
+				team.node.find('.team-smiles').text(Math.round(team.smiles / M_PER_MI));
+				if (!winnerTeam || team.smiles > winnerTeam.smiles) { winnerTeam = team; }
+				team.node.append($('<img class="avatar" src="img/avatar/' + user.slug + '.jpg" />'));
+			}
+			winnerTeam.node.find('.team-smiles').addClass('team-winner');
 		}
 		else {
 			var winner = null;
